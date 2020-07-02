b3a3e5ff58c6245d63580db9021abba610789fb6
parkernilson <nilsopar@gmail.com>
2020-07-01 20:06:00 -0600

    add backup of old site to backups, add picture of women

---
+++ backup/InquiryForm.php
@@ -0,0 +1,36 @@
+<div class="inquiry-form">
+    <div class="form-section-1">
+        <div class="form-flex-row">
+            [text* your-name placeholder "Name"]
+            [tel* your-phone-number placeholder "Phone Number"]
+        </div>
+        <div class="form-flex-row">
+            [text* your-email placeholder "Enter your email"]
+        </div>
+    </div>
+    <div class="form-section-2">
+        <div class="option-1">
+            [text* your-rental-service placeholder "Your Airbnb/VRBO Url"]
+        </div>
+        <div class="separator">
+            <p class="or">Or</p>
+        </div>
+        <div class="option-2">
+            <div class="form-flex-row">
+                [text* your-bedroom-num placeholder "Bedroom #"]
+                [text* your-yr-build placeholder "Yr Build"]
+            </div>
+            <div class="form-flex-row">
+                [text* your-bathroom-num placeholder "Bathroom #"]
+                [text* your-city placeholder "City"]
+            </div>
+            <div class="form-flex-row">
+                [text* your-sq-ft placeholder "Sq Ft"]
+                [text* your-zip-code placeholder "Zip Code"]
+            </div>
+        </div>
+    </div>
+    <div class="submit-container">
+        [submit "Submit"]
+    </div>
+</div>
